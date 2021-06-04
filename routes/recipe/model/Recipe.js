3c4a810f116c9b985eaea01493e7d3955f7a69bd
Alondradaisy <alondra.warren96@gmail.com>
2021-06-03 18:56:08 -0700

    add to recipe.js

--- routes/recipe/model/Recipe.js
+++ routes/recipe/model/Recipe.js
@@ -1,1 +1,11 @@
-let recipe = 
+const recipe = require('../model/recipe');
+const bcrypt = require('bcrypt');
+
+const mongoose = require('mongoose');
+
+const Recipe = new mongoose.recipe({
+    RecipeName: {
+        type: String
+    },
+});
+
